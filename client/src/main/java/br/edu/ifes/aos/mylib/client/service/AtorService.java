f35f63ae81137ae56c4a2bcd6074ef0a45d7c17f
Chrysthian Moizes <chrysthian.moizes@basis.com.br>
2021-05-13 20:31:18 -0300

    Criaçao do client

---
+++ client/src/main/java/br/edu/ifes/aos/mylib/client/service/AtorService.java
@@ -0,0 +1,25 @@
+package br.edu.ifes.aos.mylib.client.service;
+
+import br.edu.ifes.aos.mylib.client.service.dto.AtorDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AtorService {
+
+    public List<AtorDTO> listar() {
+        return null;
+    }
+
+    public AtorDTO salvar(AtorDTO atorDTO) {
+        return null;
+    }
+
+    public void excluir(Long id) {
+
+    }
+
+}
---
+++ client/src/main/java/br/edu/ifes/aos/mylib/client/service/feign/MylibService.java
@@ -0,0 +1,7 @@
+package br.edu.ifes.aos.mylib.client.service.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient()
+public interface MylibService {
+}
