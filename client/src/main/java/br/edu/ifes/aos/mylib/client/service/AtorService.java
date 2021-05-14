565800d8ba24eecc98f5ddee93870f5fce11aa1b
Chrysthian Moizes <chrysthian.moizes@basis.com.br>
2021-05-13 21:43:23 -0300

    Configuraçao feign

--- client/src/main/java/br/edu/ifes/aos/mylib/client/service/AtorService.java
+++ client/src/main/java/br/edu/ifes/aos/mylib/client/service/AtorService.java
@@ -1,6 +1,7 @@
 package br.edu.ifes.aos.mylib.client.service;
 
 import br.edu.ifes.aos.mylib.client.service.dto.AtorDTO;
+import br.edu.ifes.aos.mylib.client.service.feign.MylibService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,16 +11,18 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AtorService {
 
+    private final MylibService mylibService;
+
     public List<AtorDTO> listar() {
-        return null;
+        return mylibService.listar();
     }
 
     public AtorDTO salvar(AtorDTO atorDTO) {
-        return null;
+        return mylibService.salvar(atorDTO);
     }
 
     public void excluir(Long id) {
-
+        mylibService.excluir(id);
     }
 
 }
--- client/src/main/java/br/edu/ifes/aos/mylib/client/service/feign/MylibService.java
+++ client/src/main/java/br/edu/ifes/aos/mylib/client/service/feign/MylibService.java
@@ -1,7 +1,28 @@
 package br.edu.ifes.aos.mylib.client.service.feign;
 
+import br.edu.ifes.aos.mylib.client.service.dto.AtorDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient()
+import java.util.List;
+
+@FeignClient(name = "mylibservice", url = "${application.feign.url}")
 public interface MylibService {
+
+    @GetMapping("/api/atores")
+    List<AtorDTO> listar();
+
+    @PostMapping("/api/atores")
+    AtorDTO salvar(@RequestBody AtorDTO atorDTO);
+
+    @PutMapping("/api/atores")
+    AtorDTO editar(@RequestBody AtorDTO atorDTO);
+
+    @DeleteMapping("/api/atores/{id}")
+    void excluir(@PathVariable Long id);
 }
